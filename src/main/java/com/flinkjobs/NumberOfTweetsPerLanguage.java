36664fb0ec5098d857c312741ba91bab3e799a76
kochuv <veronika.kochugova@gmail.com>
2020-03-30 02:26:34 +0300

    wip

---
+++ src/main/java/com/flinkjobs/NumberOfTweetsPerLanguage.java
@@ -0,0 +1,8 @@
+package com.flinkjobs;
+
+
+public class NumberOfTweetsPerLanguage {
+    public static void main(String[] args) {
+        final Strea
+    }
+}
---
+++ src/main/java/com/flinkjobs/TestJob.java
@@ -0,0 +1,5 @@
+package com.flinkjobs;
+
+public class TestJob {
+
+}
